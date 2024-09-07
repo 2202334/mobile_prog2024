627e13261e11f4d4f701a60ce66dec2a182091ed
Margarette <2202334@ub.edu.ph>
2024-09-07 11:16:05 +0800

    first commit

---
+++ play_demogitchange.js
@@ -0,0 +1,23 @@
+console.log("Demo GIT Change");
+
+// ADD, CONTACT AND PUSH
+/*
+
+1. Create a new repository on github
+2. Go to the folder where your project is located
+3. Run git init
+4. Run git add 
+5. Run git commit -m"Initial commit"
+6. Run git Branch -M main
+7. Run git remote add origin <url>
+8. Run git push -u origin main
+
+*/
+
+// ADD/EDIT, COMMIT AND PUSH
+/*
+1. git add .
+2. git commit -m"Demo GIT change"
+3. git push 
+
+*/
